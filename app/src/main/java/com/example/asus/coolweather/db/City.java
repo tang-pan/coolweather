f5d04dfd53524a8e2474526d8705aeab142545ad
tang-pan <1044639436@qq.com>
2019-01-21 17:06:29 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/example/asus/coolweather/db/City.java
@@ -0,0 +1,52 @@
+package com.example.asus.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * 市
+ * Created by asus on 2019/1/21.
+ */
+
+public class City extends DataSupport {
+
+    private int id;
+
+    private String cityName;
+
+    private int cityCode;
+
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+}
---
+++ app/src/main/java/com/example/asus/coolweather/db/Province.java
@@ -0,0 +1,42 @@
+package com.example.asus.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * 省
+ * Created by asus on 2019/1/21.
+ */
+
+public class Province extends DataSupport {
+
+    private int id;
+
+    private String provinceName;
+
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+}
